264f96e9a23370f53a858d8e3077077b09b58fb3
Shachar Gluska <shacharg@hailo@ai>
2021-11-02 15:34:48 +0000

    Added exportable version for PatchEmbed

--- timm/models/layers/patch_embed.py
+++ timm/models/layers/patch_embed.py
@@ -37,3 +37,56 @@ class PatchEmbed(nn.Module):
             x = x.flatten(2).transpose(1, 2)  # BCHW -> BNC
         x = self.norm(x)
         return x
+
+    def fuse_bn(self):
+        if not self.flatten:
+            return self
+
+        embed_dim, in_chans, _, _ = self.proj.weight.size()
+        new_layer = PatchEmbedAsConv1x1(
+            self.img_size[0], self.patch_size, in_chans, embed_dim)
+        new_layer.norm = self.norm
+
+        weights = self.proj.weight.view(
+            embed_dim, self.patch_size[0]*self.patch_size[1]*in_chans, 1, 1)
+        bias = self.proj.bias
+
+        new_layer.proj.weight = nn.Parameter(weights)
+        new_layer.proj.bias = nn.Parameter(bias)
+
+        return new_layer
+
+
+class PatchEmbedAsConv1x1(nn.Module):
+    """ 2D Image to Patch Embedding
+    """
+
+    def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=768, norm_layer=None):
+        super().__init__()
+        img_size = to_2tuple(img_size)
+        patch_size = to_2tuple(patch_size)
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.grid_size = (img_size[0] // patch_size[0],
+                          img_size[1] // patch_size[1])
+        self.num_patches = self.grid_size[0] * self.grid_size[1]
+
+        self.proj = nn.Conv2d(in_chans, embed_dim,
+                              kernel_size=1, stride=1)
+        self.norm = norm_layer(embed_dim) if norm_layer else nn.Identity()
+
+    def forward(self, x):
+        B, C, H, W = x.shape
+        assert H == self.img_size[0] and W == self.img_size[1], \
+            f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
+        x = x.view(B, C, self.grid_size[0], self.patch_size[0],
+                   self.grid_size[1], self.patch_size[1])
+        x = x.permute(0, 1, 3, 5, 2, 4)
+        x = x.reshape(B,
+                      C * self.patch_size[0] * self.patch_size[1],
+                      self.num_patches,
+                      1)
+        x = self.proj(x)
+        x = x.flatten(2).transpose(1, 2)  # BCHW -> BNC
+        x = self.norm(x)
+        return x
