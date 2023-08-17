ca944eaee38bb585cab3c392bbe5b17affbd1320
sholevs66 <45199264+sholevs66@users.noreply.github.com>
2023-08-17 14:54:48 +0300

    Update vision_transformer.py
    For parsing - replace bind with a simple indexation

--- timm/models/vision_transformer.py
+++ timm/models/vision_transformer.py
@@ -80,7 +80,7 @@ class Attention(nn.Module):
     def forward(self, x):
         B, N, C = x.shape
         qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
-        q, k, v = qkv.unbind(0)
+        q, k, v = qkv[0], qkv[1], qkv[2]
         q, k = self.q_norm(q), self.k_norm(k)
 
         if self.fused_attn:
